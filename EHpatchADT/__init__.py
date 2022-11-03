835091851fc50d3eb92ac44ed7b42c20ef8988ca
billmanh <william.jeffrey.harding@gmail.com>
2022-11-03 12:08:44 -0700

    updated EH for more basic request

--- EHpatchADT/__init__.py
+++ EHpatchADT/__init__.py
@@ -4,5 +4,4 @@ import azure.functions as func
 
 
 def main(event: func.EventHubEvent):
-    logging.info('Python EventHub trigger processed an event: %s',
-                 event.get_body().decode('utf-8'))
+    logging.info('*********** Process has begun **************')
